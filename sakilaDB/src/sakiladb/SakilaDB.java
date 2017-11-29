accb593fbefdd7f4f50bbcb510b049883efefb98
ChrisDal <33423995+chrisdalamitras@users.noreply.github.com>
2017-11-29 13:22:32 +0200

    Add files via upload

--- sakilaDB/src/sakiladb/SakilaDB.java
+++ sakilaDB/src/sakiladb/SakilaDB.java
@@ -19,23 +19,62 @@ import java.util.logging.Logger;
  */
 public class SakilaDB {
 
-       private static Connection conn = null;
-       private static Statement stmt = null;
-       private static ResultSet rs = null;
        private static final String DB_URL = "jdbc:mysql://localhost:3306/sakila";
        private static final String USER = "root";
        private static final String PASS = "chris"; 
     
     public static void main(String[] args) {
         
+       Connection conn = null;
+       Statement stmt = null; 
+       ResultSet rs = null; 
+       
        try { 
             Class.forName("com.mysql.jdbc.Driver");
             System.out.println("Connecting to database.");
             conn = DriverManager.getConnection(DB_URL, USER, PASS);
+            System.out.println("Creating statement");
             stmt = conn.createStatement( );
+            String sql;
+            sql = " SELECT actor_id, first_name, last_name FROM actor";
+            System.out.println("Excecuting statement");
+            rs = stmt.executeQuery(sql);
+            
+            while(rs.next()){
+                int actor_id = rs.getInt("actor_id");
+                String firstname = rs.getString("first_name");
+                String lastname = rs.getString("last_name");
+                
+                System.out.print("ActorID: "+ actor_id);
+                System.out.print(", First name: " + firstname);
+                System.out.println(", Last name: "+ lastname);
+            }
+            
+            rs.close();
+            stmt.close();
+            conn.close();
+            
         } catch (ClassNotFoundException | SQLException ex) {
             Logger.getLogger(SakilaDB.class.getName()).log(Level.SEVERE, null, ex);
-        }    
+        }finally{
+           
+           try{
+               if(stmt != null)
+                   stmt.close();
+           }catch(SQLException se2){
+               
+           }
+           try{
+               if(conn != null)
+                   conn.close();                              
+           }catch(SQLException se){
+               
+               se.printStackTrace();              
+           }           
+       } 
+        
+       System.out.println("Finished!");
+       
     }
     
     
